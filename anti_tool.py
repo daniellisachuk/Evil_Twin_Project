ca8768ee630dde5e720d0c0447b241326a4e6d8b
daniellisachuk <daniellisachuk@gmail.com>
2020-10-09 13:59:36 -0400

    print fixed

--- anti_tool.py
+++ anti_tool.py
@@ -1,7 +1,7 @@
 from scapy.all import sniff
+from colorama import init, Fore
 
-
-
+init()
 
 count = 0
 
@@ -12,6 +12,6 @@ def sniff_deauth(pkt):
         if pkt.type == 0 and pkt.subtype == 0xc:
 	    count = count + 1
     if count >= 20:
-        print("warning you'r AP is under deauth-attack")
+        print(f"{Fore.RED}WARNING! -\nNumber of deauth packets in the system has exeeded the threshhold\n Your AP might be under deauth-attack{Fore.RESET}")
 
 sniff(iface="wlan0", prn = sniff_deauth)
